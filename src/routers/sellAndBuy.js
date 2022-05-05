05bb86e805641cd9196e0b974adac89525036b4c
[Ambattz} <[alvingeorge30@gmail.com]>
2022-05-05 12:34:45 +0530

    Initial Commit

---
+++ src/routers/sellAndBuy.js
@@ -0,0 +1,16 @@
+const e = require("express");
+const express = require("express");
+const SellBuy =require("../mongoose/models/sellBuy")
+
+// setting up the router
+
+const sellAndBuyRouter = new express.Router();
+
+// code goes here for routes
+
+
+// exporting the router
+
+module.exports = sellAndBuyRouter;
+
+
