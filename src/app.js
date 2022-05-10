d2486b40cb2daa67e200bfaa11e96389abc0caa0
[Ambattz} <[alvingeorge30@gmail.com]>
2022-05-10 12:30:24 +0530

    endpoints 1/4 done

--- src/app.js
+++ src/app.js
@@ -12,15 +12,11 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "GET,DELETE,PATCH");
-      return res.status(200).json({});
+    res.header("Access-Control-Allow-Methods", "GET,DELETE,PATCH");
+    return res.status(200).json({});
   }
   next();
 });
 app.use(sellAndBuyRouter);
 
 module.exports = app;
-
-
-  
-  
--- src/routers/sellAndBuy.js
+++ src/routers/sellAndBuy.js
@@ -1,16 +1,70 @@
 const e = require("express");
 const express = require("express");
-const SellBuy =require("../mongoose/models/sellBuy")
+const SellBuy = require("../mongoose/models/sellBuy")
 
 // setting up the router
-
 const sellAndBuyRouter = new express.Router();
 
 // code goes here for routes
-
+//post method for sellProduct
+sellAndBuyRouter.get("/sellProduct", (req, res) => {
+    const newProduct = new SellBuy({
+        productName: req.body.productName,
+        costPrice: req.body.costPrice
+    })
+    newProduct
+        .save()
+        .then((result) => {
+            res.status(201).send({ result: result, message: "Product Added" })
+        })
+        .catch((err) => {
+            if (err.name == 'ValidationError') {
+                for (field in err.errors) {
+                    res.status(400).send(err.errors[field].message);
+                }
+            }
+        });
+})
+//post method for sellProduct
+sellAndBuyRouter.post("/sellProduct", (req, res) => {
+    const newProduct = new SellBuy({
+        productName: req.body.productName,
+        costPrice: req.body.costPrice
+    })
+    newProduct
+        .save()
+        .then((result) => {
+            res.status(201).send({ result: result, message: "Product Added" })
+        })
+        .catch((err) => {
+            if (err.name == 'ValidationError') {
+                for (field in err.errors) {
+                    res.status(400).send(err.errors[field].message);
+                }
+            }
+        });
+})
+//Patch method for sellProduct:id
+sellAndBuyRouter.get("/sellProduct:id", (req, res) => {
+    const newProduct = new SellBuy({
+        productName: req.body.productName,
+        costPrice: req.body.costPrice
+    })
+    newProduct
+        .save()
+        .then((result) => {
+            res.status(201).send({ result: result, message: "Product Added" })
+        })
+        .catch((err) => {
+            if (err.name == 'ValidationError') {
+                for (field in err.errors) {
+                    res.status(400).send(err.errors[field].message);
+                }
+            }
+        });
+})
 
 // exporting the router
-
 module.exports = sellAndBuyRouter;
 
 
